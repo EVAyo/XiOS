29883251ca7afbd1e39ec1d291d55c7e8f88414f
lionsom <lionsom_lin@qq.com>
2021-10-15 15:45:37 +0800

    Swift

--- iOS/Swift/Swift基础学习/MyPlayground.playground/Pages/泛型.xcplaygroundpage/Contents.swift
+++ iOS/Swift/Swift基础学习/MyPlayground.playground/Pages/泛型.xcplaygroundpage/Contents.swift
@@ -17,3 +17,28 @@ swapValues(&i1, &i2)
 var d1 = 10.0
 var d2 = 20.0
 swapValues(&d1, &d2)
+
+
+func GCDTest2() {
+        let myQueue = DispatchQueue(label: "com.myQueue", qos: .default, attributes: .concurrent, autoreleaseFrequency: .workItem, target: nil)//并行队列
+        myQueue.sync {//任务一
+            for _ in 0...10 {
+                 print("任务1......")
+            }
+        }
+        myQueue.sync {
+            for _ in 0...5 {
+                print("任务2++++++")
+            }
+        }
+//        // barrier 会等待上面执行完毕再执行下面的，会阻塞当前线程
+//        myQueue.async(group: nil, qos: .default, flags: .barrier, execute: {//2.
+//            print("000000")
+//        })
+//
+//        myQueue.async {
+//            print("111111")
+//        }
+    }
+
+GCDTest2()
